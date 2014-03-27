26c7096a28e7deb29fc2a3cae215f9b155ab8da6
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2014-03-27 09:30:27 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/NeuroProfit@586 302f0312-3c5d-0d4b-b714-136c201f073e

--- NeuroProfitUI/IndicatorForm.Designer.cs
+++ NeuroProfitUI/IndicatorForm.Designer.cs
@@ -26,9 +26,9 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -73,16 +73,6 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(501, 256);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.button1.Location = new System.Drawing.Point(417, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // tableLayoutPanel3
             // 
             this.tableLayoutPanel3.ColumnCount = 2;
@@ -108,14 +98,25 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(417, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // IndicatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(728, 262);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
+            this.Name = "IndicatorForm";
             this.Text = "IndicatorForm";
+            this.Load += new System.EventHandler(this.IndicatorForm_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
--- NeuroProfitUI/IndicatorForm.cs
+++ NeuroProfitUI/IndicatorForm.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using NeuroProfit.Indicators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,5 +15,14 @@ namespace NeuroProfitUI {
 		public IndicatorForm() {
 			InitializeComponent();
 		}
+
+        private void IndicatorForm_Load(object sender, EventArgs e) {
+            var indicators = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(Indicator))).Select(t => Activator.CreateInstance(t)).Cast<Indicator>();
+            var classInfos = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(ClassInfo))).Select(t => Activator.CreateInstance(t)).Cast<ClassInfo>();
+
+            foreach (var indicatorType in indicators) {
+                if (classInfos.FirstOrDefault(ci => ci.
+            }
+        }
 	}
 }
