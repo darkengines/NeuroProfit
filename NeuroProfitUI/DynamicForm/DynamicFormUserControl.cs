2d02994f5d31791ebffa72b5cc8e00e8e45fcab7
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2014-03-29 02:33:23 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/NeuroProfit@590 302f0312-3c5d-0d4b-b714-136c201f073e

--- NeuroProfitUI/DynamicForm/DynamicFormUserControl.cs
+++ NeuroProfitUI/DynamicForm/DynamicFormUserControl.cs
@@ -7,73 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NeuroProfit.Indicators;
 
 namespace NeuroProfitUI.DynamicForm {
-	public partial class DynamicFormUserControl : UserControl {
-		public ClassInfo classInfo;
-		protected Dictionary<MemberInfo, DynamicFormEditor> MembersControlMap { get; set; }
-		protected object instance;
-		public ClassInfo ClassInfo {
-			get { return classInfo; }
-			set {
-				if (value != null && value != classInfo) {
-					classInfo = value;
-					instance = Activator.CreateInstance(classInfo.GetType().BaseType.GenericTypeArguments[0]);
-					Build();
-					FillForm();
-				}
-			}
-		}
-		public DynamicFormUserControl() {
-			MembersControlMap = new Dictionary<MemberInfo, DynamicFormEditor>();
-			InitializeComponent();
-		}
+	public class DynamicFormUserControl : UserControl {
 		public object Instance {
-			get {
-				FillInstance();
-				return instance;
-			}
+			get;
+			set;
 		}
-		protected void Build() {
-			MembersControlMap.Clear();
-			DynamicLayout.Controls.Clear();
-			var members = classInfo.Members.OrderBy(m => m.Position);
-			DynamicLayout.ColumnCount = 2;
-			DynamicLayout.ColumnStyles[0].SizeType = SizeType.AutoSize;
-			DynamicLayout.ColumnStyles.Add(new ColumnStyle());
-			DynamicLayout.ColumnStyles[1].SizeType = SizeType.Percent;
-			DynamicLayout.ColumnStyles[1].Width = 100;
-			DynamicLayout.RowCount = members.Count();
-			var i = 0;
-			foreach (var member in members) {
-				if (DynamicLayout.RowStyles.Count < i + 1) {
-					DynamicLayout.RowStyles.Add(new RowStyle());
+		protected void BuildForm() {
+			var properties = Instance.GetType().GetProperties().Where(p => Attribute.IsDefined(p, typeof(ParameterAttribute)));
+			foreach (var property in properties) {
+				var dataSourceType = property.GetCustomAttributes(typeof(ParameterAttribute), false)
+					.Cast<ParameterAttribute>()
+					.First()
+					.DataSourceProviderType;
+				if (dataSourceType != null) {
+					var dataSource = Activator.CreateInstance(dataSourceType);
+				} else {
+
 				}
-				DynamicLayout.RowStyles[i].SizeType = SizeType.Absolute;
-				DynamicLayout.RowStyles[i].Height = 32;
-				var label = new Label();
-				label.Name = string.Format("label_{0}", member.Name);
-				label.Text = member.Label;
-				label.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
-				var editor = (Control)member.Editor;
-				editor.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
-				editor.Name = string.Format("editor_{0}", member.Name);
-				DynamicLayout.Controls.Add(label, 0, i);
-				DynamicLayout.Controls.Add(editor, 1, i);
-				MembersControlMap[member] = (DynamicFormEditor)editor;
-				i++;
-			}
-		}
-		protected void FillForm() {
-			foreach (var member in MembersControlMap.Keys) {
-				var editor = MembersControlMap[member];
-				editor.SetValue(instance.GetType().GetProperty(member.Name).GetValue(instance));
-			}
-		}
-		protected void FillInstance() {
-			var type = instance.GetType();
-			foreach (var member in MembersControlMap.Keys) {
-				type.GetProperty(member.Name).SetValue(instance, MembersControlMap[member].GetValue());
 			}
 		}
 	}
